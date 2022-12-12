8f02ff0f15020c221e31c8693216598cca692c4a
KimJungWoo <86278689+tiktakz@users.noreply.github.com>
2022-12-12 22:53:44 +0900

    CITA 채팅 완성

---
+++ public/firebase_config.js
@@ -0,0 +1,12 @@
+// FIREBASE CONFIG    
+const firebaseConfig = {
+    apiKey: "AIzaSyDAviFAwj0F60qzBjhGymUL-hzpErarvl8",
+    authDomain: "cita-a057c.firebaseapp.com",
+    databaseURL: "https://cita-a057c-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "cita-a057c",
+    storageBucket: "cita-a057c.appspot.com",
+    messagingSenderId: "583943011318",
+    appId: "1:583943011318:web:98f9864c4557a420fc25d7",
+    measurementId: "G-35W2MMH9S8"
+};
+firebase.initializeApp(firebaseConfig);
---
+++ public/scripts/chat.js
@@ -0,0 +1,62 @@
+function red() {
+  var div = document.getElementById('card'); // id가 card인 객체 찾기
+  div.style.background = '#FFA9A9';
+  var color = document.getElementById('color');
+  color.value = div.style.background;
+  var cat = document.getElementById('category');
+  // cat.value = 
+}
+function select1() {
+  var div = document.getElementById('button');
+  var backs = document.getElementById('back');
+  backs.style.background =  "linear-gradient(to right, #fa709a 0%, #fee140 100%)";
+  div.style.backgroundimage = "linear-gradient(to right, #fa709a 0%, #fee140 100%)";
+  var log = div.style.backgroundimage;
+  var color = document.getElementById('color');
+  color.value = log;
+}
+function select2() {
+  var div = document.getElementById('button');
+  var backs = document.getElementById('back');
+  backs.style.background =  "linear-gradient(to top, #5ee7df 0%, #b490ca 100%)";
+  div.style.backgroundimage = "linear-gradient(to top, #5ee7df 0%, #b490ca 100%)";
+  var log = div.style.backgroundimage;
+  var color = document.getElementById('color');
+  color.value = log;
+}
+function select3() {
+  var div = document.getElementById('button');
+  var backs = document.getElementById('back');
+  backs.style.background =  "linear-gradient(to top, #5fa919 0%, #802eb3 100%)";
+  div.style.backgroundimage = "linear-gradient(to top, #5fa919 0%, #802eb3 100%)";
+  var log = div.style.backgroundimage;
+  var color = document.getElementById('color');
+  color.value = log;
+}
+function select4() {
+  var div = document.getElementById('button');
+  var backs = document.getElementById('back');
+  backs.style.background =  "linear-gradient(to top, #c8a1bd 0%, #0fd4309c 100%)";
+  div.style.backgroundimage = "linear-gradient(to top, #c8a1bd 0%, #0fd4309c 100%)";
+  var log = div.style.backgroundimage;
+  var color = document.getElementById('color');
+  color.value = log;
+}
+function select5() {
+  var div = document.getElementById('button');
+  var backs = document.getElementById('back');
+  backs.style.background =  "linear-gradient(to top, #c3e23be3 0%, #0a7256 100%)";
+  div.style.backgroundimage = "linear-gradient(to top, #c3e23be3 0%, #0a7256 100%)";
+  var log = div.style.backgroundimage;
+  var color = document.getElementById('color');
+  color.value = log;
+}
+function select6() {
+  var div = document.getElementById('button');
+  var backs = document.getElementById('back');
+  backs.style.background =  "linear-gradient(to top, #590969 0%, #d1e8d8 100%)";
+  div.style.backgroundimage = "linear-gradient(to top, #590969 0%, #d1e8d8 100%)";
+  var log = div.style.backgroundimage;
+  var color = document.getElementById('color');
+  color.value = log;
+}
